9c0c7b51b6d5c053f32271f9a8530d7ef45fb8b2
Dharsana <dharsanamani123@gmail.com>
2026-07-15 12:25:03 +0530

    15/07/2026

--- 7RMartSuperMarket/src/main/java/constant/Constant.java
+++ 7RMartSuperMarket/src/main/java/constant/Constant.java
@@ -3,4 +3,5 @@ package constant;
 public class Constant {
 	public static final String TESTDATA=System.getProperty("user.dir")+"\\src\\test\\resources\\TestDatas.xlsx";
 public static final String APPLEIMG=System.getProperty("user.dir")+"\\src\\test\\resources\\Screenshot 2026-07-06 203937.png";
+public static final String CONFIG=System.getProperty("user.dir")+"\\src\\main\\resources\\Config.properties";
 }
--- 7RMartSuperMarket/src/test/java/testscript/Base.java
+++ 7RMartSuperMarket/src/test/java/testscript/Base.java
@@ -1,7 +1,10 @@
 package testscript;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,13 +13,25 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import constant.Constant;
 import utilities.ScreenShotUtility;
 import utilities.WaitUtility;
 public class Base {
 	public WebDriver driver;
+	Properties pr;
+	FileInputStream fileinput;
 	@BeforeMethod(alwaysRun=true)
 	@Parameters("browser")
 	public void initializeBrowser(String browser) throws Exception {
+		try {
+			pr=new Properties();
+			fileinput=new FileInputStream(Constant.CONFIG);
+		pr.load(fileinput);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 		if(browser.equalsIgnoreCase("chrome"))
 		{
 			driver = new ChromeDriver();
@@ -28,7 +43,8 @@ public class Base {
 		else {
 			throw new Exception("Invalid Browser");
 		}
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		//driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		driver.get(pr.getProperty("url"));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitUtility.IMPLICITWAIT));
 		driver.manage().window().maximize();
 	}
